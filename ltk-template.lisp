04912dc2ee372f2709b182defdce359b8a2d5dbe
RyanBurnside <pixeloutlaw@gmail.com>
2019-10-09 22:06:31 -0600

    Major restructuring, still more to do.

--- ltk-template.lisp
+++ ltk-template.lisp
@@ -12,24 +12,28 @@
   (let* ((hull (make-instance 'frame :master *tk*))
 	 (canvas (make-instance 'canvas
 				:master hull
-				:width 256
-				:height 320))
-	 (input-hull (make-instance 'labelframe :master hull :text "Tile Attributes"))
-	 
-	 (name-label (make-instance 'label :master input-hull :text "Name: "))
-	 (name-entry (make-instance 'entry :master input-hull))
-	 (name-id-label (make-instance 'label :master input-hull :text "-"))
-	 (name-id-entry (make-instance 'spinbox :master input-hull :width 4 :from 0 :to 9999 :increment 1))
-	 (x-label (make-instance 'label :master input-hull :text "X: "))
-	 (x-entry (make-instance 'spinbox :master input-hull :width 4 :from 0 :increment 8 :to 9999))
-	 (y-label (make-instance 'label :master input-hull :text "Y: "))
-	 (y-entry (make-instance 'spinbox :master input-hull :width 4 :from 0 :increment 8 :to 9999))
-	 (width-label (make-instance 'label :master  input-hull :text "Width: "))
-	 (width-entry (make-instance 'entry :master  input-hull :width 4 :text 8))
-	 (height-label (make-instance 'label :master input-hull :text "Height: "))
-	 (height-entry (make-instance 'entry :master input-hull :width 4 :text 8))
-	 (comments-label (make-instance 'label :master input-hull :text "Comments: "))
-	 (comments (make-instance 'text :master input-hull :width 40 :height 4 :wrap "WORD"))
+				:width 480
+				:height 480))
+
+
+	 (animation-hull (make-instance 'labelframe :master hull :text "Animation"))
+	 (animation-name-label (make-instance 'label :master animation-hull :text "Name: "))
+	 (animation-name (make-instance 'spinbox :master animation-hull))
+	 (tile-hull (make-instance 'labelframe :master animation-hull :text "Tile"))
+	 (tile-button-previous (make-instance 'button :master tile-hull :text "<--" :width 5))
+	 (tile-button-next (make-instance 'button :master tile-hull :text "-->" :width 5))
+	 (name-label (make-instance 'label :master tile-hull :text "Name: "))
+	 (name-entry (make-instance 'entry :master tile-hull))
+	 (name-id-label (make-instance 'label :master tile-hull :text "-"))
+	 (name-id-entry (make-instance 'spinbox :master tile-hull :width 4 :from 0 :to 9999 :increment 1))
+	 (x-label (make-instance 'label :master tile-hull :text "X: "))
+	 (x-entry (make-instance 'spinbox :master tile-hull :width 4 :from 0 :increment 8 :to 9999))
+	 (y-label (make-instance 'label :master tile-hull :text "Y: "))
+	 (y-entry (make-instance 'spinbox :master tile-hull :width 4 :from 0 :increment 8 :to 9999))
+	 (width-label (make-instance 'label :master  tile-hull :text "Width: "))
+	 (width-entry (make-instance 'entry :master  tile-hull :width 4 :text 8))
+	 (height-label (make-instance 'label :master tile-hull :text "Height: "))
+	 (height-entry (make-instance 'entry :master tile-hull :width 4 :text 8))
 	 (grid-hull (make-instance 'labelframe :master hull :text "Grid"))
 	 (grid-checkbox (make-instance 'check-button :master
 				       grid-hull :text "Snap"))
@@ -44,14 +48,16 @@
     
     (grid hull 0 0)
     (grid canvas 0 0 :sticky "NW" :rowspan 2)
-    (grid input-hull 0 1 :sticky "NW")
-
-    
+    (grid animation-hull 0 1 :sticky "NW")
+    (grid animation-name-label 0 0 :sticky "NE")
+    (grid animation-name 0 1 :sticky "NW")
+    (grid tile-hull 2 0 :sticky "NW" :columnspan 99)
+    (grid tile-button-previous 5 0 :sticky "NW")
+    (grid tile-button-next 5 1 :sticky "NW")
     (grid name-label 0 1 :sticky "NE")
     (grid name-entry 0 2 :sticky "NW")
     (grid name-id-label 0 3 :sticky "NW")
     (grid name-id-entry 0 4 :sticky "NW")
-
     (grid x-label 1 1 :sticky "NE")
     (grid x-entry 1 2 :sticky "NW")
     (grid y-label 2 1 :sticky "NE")
@@ -60,9 +66,6 @@
     (grid width-entry 3 2 :sticky "NW")
     (grid height-label 4 1 :sticky "NE")
     (grid height-entry 4 2 :sticky "NW")
-    (grid comments-label 5 1 :sticky "NE")
-    (grid comments 5 2 :sticky "NW" :columnspan 3)
-
     (grid grid-hull 1 1 :sticky "NW")
     (grid grid-checkbox 0 0 :sticky "NE")
     (grid grid-square 0 1 :sticky "NW")
