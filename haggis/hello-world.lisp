2d85d65a13a0ea5209047d04e485b3d20eb4a423
Peter Keller <psilord@cs.wisc.edu>
2019-11-07 01:48:52 -0600

    Added handlers for destroy and delete-event of the window itself.

--- haggis/hello-world.lisp
+++ haggis/hello-world.lisp
@@ -19,8 +19,25 @@
        (gtk:gtk-builder-connect-signals
         builder `(("quit-button-clicked" clicked-callback)))
 
+
        (setf top-level (gtk:gtk-builder-get-object builder "top_level"))
        (format t "Created toplevel ~A~%" top-level)
+
+       ;; Signal handler for the window to handle the signal "destroy".
+       (gobject:g-signal-connect
+        top-level "destroy"
+        (lambda (widget)
+          (declare (ignore widget))
+          (format t "Destroy Event Occured.~%")
+          (gtk:leave-gtk-main)))
+
+       ;; Signal handler for the window to handle the signal "delete-event".
+       (gobject:g-signal-connect
+        top-level "delete-event"
+        (lambda (widget event)
+          (declare (ignore widget event))
+          (format t "Delete Event Occured (and caught and ignored).~%")
+          gdk::+gdk-event-stop+))
 
        (gtk:gtk-widget-show-all top-level)))
 
